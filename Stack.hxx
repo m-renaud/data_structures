a3b9b81dc7ee00769b0616d9fb3a23f1f93f8d2c
Matt Renaud <mrenaud92@gmail.com>
2011-07-29 08:39:19 -0400

    Created a Stack header

---
+++ Stack.hxx
@@ -0,0 +1,48 @@
+#ifndef __MRR_STACK_HXX__
+#define __MRR_STACK_HXX__
+
+template <typename T>
+struct Node
+{
+  T datum_;
+  Node<T> * next_;
+
+  Node() : datum_(0), next_(nullptr) { }
+  Node(T const& val) : datum_(val), next_(nullptr) { }
+  Node(Node<T> const& n) : datum_(n.datum_), next_(n.next_) { }
+};
+
+template <typename T>
+struct Stack
+{
+  Node<T> * head_;
+
+public:
+
+  Stack() : head_(nullptr){ }
+
+  Node<T> * push(T val)
+  {
+    Node<T> * n = new Node<T>(val);
+
+    n->next_ = head_;
+    head_ = n;
+    return head_;
+  }
+
+  T pop()
+  {
+    if(head_ == nullptr)
+    {
+      std::cout << "error: No elements on stack. Exiting now." << std::endl;
+      exit(1);
+    }
+    T val = head_->datum_;
+    Node<T> * oldHead = head_;
+    head_ = head_->next_;
+    delete oldHead;
+    return val;
+  }
+};
+
+#endif // __MRR_STACK_HXX__
