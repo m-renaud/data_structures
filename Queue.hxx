b4fd97197e6c1b73655c0428ab850b9ceebc7acb
Matt Renaud <mrenaud92@gmail.com>
2011-09-01 00:18:14 -0400

    Improved Queue.hxx Added typedefs for value_type Added const& where able. Moved function definitions outside class.

--- Queue.hxx
+++ Queue.hxx
@@ -1,76 +1,112 @@
 #ifndef MRR_QUEUE_HXX__
 #define MRR_QUEUE_HXX__
 
+//===========================================================================
+// Author: Matt Renaud
+// The following is an implementation of a simple queue.
+//---------------------------------------------------------------------------
+
 #include <iostream>
 
-template <typename T>
-struct Node
+//===========================================================================
+// QueueNode Class.
+//---------------------------------------------------------------------------
+template <class T>
+struct QueueNode
 {
-  T datum_;
-  Node<T> * next_;
+  typedef T value_type;
+  typedef QueueNode<value_type> Node;
 
-  Node() : datum_(T()), next_(nullptr) { }
-  Node(T const& val) : datum_(val), next_(nullptr) { }
-  Node(Node<T> const& n) : datum_(n.datum_), next_(n.next_) { }
+  value_type datum_;
+  Node * next_;
+
+  QueueNode() : datum_(value_type()), next_(nullptr) { }
+  QueueNode(value_type const& val) : datum_(val), next_(nullptr) { }
+  QueueNode(Node const& n) : datum_(n.datum_), next_(n.next_) { }
 
 };
 
-template <typename T>
+
+//===========================================================================
+// Queue Class.
+//---------------------------------------------------------------------------
+template <class T>
 class Queue
 {
-  Node<T> * head_;
-  Node<T> * last_;
+  typedef T value_type;
+  typedef QueueNode<value_type> Node;
+
+  Node * head_;
+  Node * last_;
 
 public:
   Queue() : head_(nullptr), last_(nullptr) { }
+  Queue(Queue<value_type> const& q);
 
-  Queue(Queue<T> const& q)
-  {
-    head_ = new Node<T>(q.head_->datum_);
-    Node<T> * n = head_;
-    Node<T> * cur = q.head_->next_;
-    while(cur != nullptr)
-    {
-      n->next_ = new Node<T>(cur->datum_);
-      cur = cur->next_;
-      n = n->next_;
-    }
-    last_ = n;
-  }
-
-  bool empty()
-  {
-    return head_ == nullptr;
-  }
-
-  Node<T> * enqueue(T val)
-  {
-    Node<T> * n = new Node<T>(val);
-
-    if(head_ == nullptr)
-    {
-      head_ = n;
-      last_ = n;
-    }
-
-    last_->next_ = n;
-    last_ = n;
-  }
-
-  T dequeue()
-  {
-    if(head_ == nullptr)
-    {
-      std::cout << "error: No elements in the queue. Exiting now." << std::endl;
-      exit(1);
-    }
-    T val = head_->datum_;
-    Node<T> * oldHead = head_;
-    head_ = head_->next_;
-    delete oldHead;
-    return val;
-  }
-
+  bool empty();
+  Node * enqueue(value_type const& val);
+  value_type dequeue();
 };
+
+
+//===========================================================================
+template <class T>
+Queue<T>::Queue(Queue<value_type> const& q)
+{
+  head_ = new Node(q.head_->datum_);
+  Node * n = head_;
+  Node * cur = q.head_->next_;
+
+  while(cur != nullptr)
+  {
+    n->next_ = new Node(cur->datum_);
+    cur = cur->next_;
+    n = n->next_;
+  }
+
+  last_ = n;
+}
+
+
+//===========================================================================
+template <class T>
+bool Queue<T>::empty()
+{
+  return head_ == nullptr;
+}
+
+
+//===========================================================================
+template <class T>
+auto Queue<T>::enqueue(value_type const& val) -> Node *
+{
+  Node * n = new Node(val);
+
+  if(head_ == nullptr)
+  {
+    head_ = n;
+    last_ = n;
+  }
+
+  last_->next_ = n;
+  last_ = n;
+}
+
+
+//===========================================================================
+template <class T>
+auto Queue<T>::dequeue() -> value_type
+{
+  if(head_ == nullptr)
+  {
+    std::cout << "error: No elements in the queue. Exiting now." << std::endl;
+    exit(1);
+  }
+  value_type val = head_->datum_;
+  Node * oldHead = head_;
+  head_ = head_->next_;
+  delete oldHead;
+  return val;
+}
 
 #endif // #ifndef MRR_QUEUE_HXX__
