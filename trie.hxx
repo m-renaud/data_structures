9e86b570642823a4f8a9d901eb5766d2abe572ab
Matt Renaud <mrenaud92@gmail.com>
2012-07-02 00:35:26 -0400

    More code cleanup on trie.hxx

--- trie.hxx
+++ trie.hxx
@@ -64,17 +64,15 @@ auto trie<Key,T>::insert(value_type const& x) -> std::pair<iterator, bool>
   node_type* cur = &root_;
   edge_list_iterator edge;
 
-  auto key_end = end(x.first);
-
   // Go through each element in the key, creating a path in the tree
-  for(auto i = begin(x.first); i != key_end; ++i)
+  for(auto const& elem : x.first)
   {
-    edge = cur->edges_.find(*i);
+    edge = cur->edges_.find(elem);
 
-    // If the current in the key doesn't exist, create it, if it does, follow 
+    // If the element in the key doesn't exist, create it, if it does, follow it
     if(edge == end(cur->edges_))
     {
-      auto newly_created = &(cur->edges_[*i]);
+      auto newly_created = &(cur->edges_[elem]);
       newly_created->parent_ = cur;
       cur = newly_created;
     }
@@ -92,17 +90,14 @@ auto trie<Key,T>::insert(value_type const& x) -> std::pair<iterator, bool>
   }
   else
   {
-  
-    // Set the key and value value at the current node and set the state to final
     cur->value_.first = x.first;
     cur->value_.second = x.second;
     cur->final_state_ = true;
 
-    // Return the value just inserted
-    return std::make_pair(trie_iterator(cur), true);
+    return std::make_pair(iterator(cur), true);
   }
 
-} // std::pair<iterator, bool> insert()
+} // std::pair<iterator, bool> trie<Key,T>::insert()
 
 
 
@@ -117,25 +112,20 @@ auto trie<Key,T>::find(key_type const& key) -> iterator
   node_type* cur = &root_;
 
   // Loop through the key
-  for(auto i = begin(key); i != end(key);++i)
+  for(auto const& elem : key)
   {
     // Follow the correct edge
-    auto edge = cur->edges_.find(*i);
+    auto edge = cur->edges_.find(elem);
 
-    // If the edge is not found, return end
+    // If the edge is not found, return end_, if it is, follow edge
     if(edge == end(cur->edges_))
       return end_;
-    // If it is, move the pointer and continue
     else
       cur = &(edge->second);
   }
 
-  // If the node we end up at is final, return a pointer to the value
-  if(cur->final_state_ == true)
-    return iterator(cur);
-  // If not, return end
-  else
-    return end_;
+  // If the node we end up at is final, return an iterator to it
+  return (cur->final_state_ == true) ? iterator(cur) : end_;
 
 } // iterator trie<Key,T>::find()
 
