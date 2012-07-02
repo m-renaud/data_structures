126649d56dc256898ed0cacba5212762742b4517
Matt Renaud <mrenaud92@gmail.com>
2012-07-01 21:43:32 -0400

    Added comments to trie.hxx

--- trie.hxx
+++ trie.hxx
@@ -78,6 +78,7 @@ public:
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Insert a key, value pair into the trie...
 template <typename Key, typename T>
 auto trie<Key,T>::insert(key_type const& key, value_type const& value)
   -> iterator
@@ -85,62 +86,86 @@ auto trie<Key,T>::insert(key_type const& key, value_type const& value)
   node_type* cur = &root_;
   typename edge_type::iterator edge;
 
+  // Go through each element in the key, creating a path in the tree
   for(auto i = std::begin(key); i != std::end(key); ++i)
   {
     edge = cur->edges_.find(*i);
+
+    // If the current in the key doesn't exist, create it
     if(edge == std::end(cur->edges_))
       cur = &(cur->edges_[*i]);
+    // If it does, follow the edge
     else
       cur =&(edge->second);
   }
 
+  // Set the value at the current node and set the state to final
   cur->value_ = value;
   cur->final_state_ = true;
 
+  // Return the value just inserted
   return &cur->value_;
 }
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Find a key in the trie...
 template <typename Key, typename T>
 auto trie<Key,T>::operator [] (key_type const& key) const -> value_type&
 {
+  // Start at the root of the trie
   node_type const* cur = &root_;
   typename edge_type::iterator edge;
 
+  // Loop through the key
   for(auto i = std::begin(key); i != std::end(key);++i)
   {
+    // Follow the correct edge
     edge = cur->edges_.find(*i);
+
+    // If the edge is not found, return end
     if(edge == std::end(cur->edges_))
       return end_;
+    // If it is, move the pointer and continue 
     else
       cur = *edge;
   }
+
+  // If the node we end up at is final, return the value
   if(cur->final_state_ == true)
     return cur->value_;
+  // If not, return end
   else
     return end_;
 }
 
 
 //m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Finds a key in the trie
 template <typename Key, typename T>
 auto trie<Key,T>::find(key_type const& key) -> iterator
 {
+  // Start at the root
   node_type * cur = &root_;
-  //  typename edge_type::iterator edge;
 
+  // Loop through the key
   for(auto i = std::begin(key); i != std::end(key);++i)
   {
+    // Follow the correct edge
     auto edge = cur->edges_.find(*i);
 
+    // If the edge is not found, return end
     if(edge == std::end(cur->edges_))
       return end_;
+    // If it is, move the pointer and continue
     else
       cur = &(edge->second);
   }
+
+  // If the node we end up at is final, return a pointer to the value
   if(cur->final_state_ == true)
     return &cur->value_;
+  // If not, return end
   else
     return end_;
 }
